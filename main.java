9dfd19788d8b1e412081692169f5bf5c17f2f3ab
Maria Garlińska <162016875+xMvasy@users.noreply.github.com>
2024-04-07 13:44:52 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        point point = new point(6.1, 5.2);
+        System.out.println("wspolrzedne punktu: x = " + point.getX() + ", y = " + point.getY());
+    }
+}
