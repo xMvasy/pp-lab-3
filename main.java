21903155e2aeb5d6c2907197393c8bb171569cab
Maria Garlińska <162016875+xMvasy@users.noreply.github.com>
2024-04-07 13:44:22 +0200

    Delete main.java

--- main.java
+++
@@ -1,6 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        point point = new point(6.1, 5.2);
-        System.out.println("wspolrzedne punktu: x = " + point.getX() + ", y = " + point.getY());
-    }
-}
