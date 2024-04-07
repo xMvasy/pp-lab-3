4e566254a20591a9ef3d1ed61bc17a19c2d85d45
Maria Garlińska <162016875+xMvasy@users.noreply.github.com>
2024-04-07 11:40:28 +0200

    ((((((((((((((((((:

---
+++ main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(3.5, 4.2);
+        System.out.println("Współrzędne punktu: (" + point.getX() + ", " + point.getY() + ")");
+    }
+}
